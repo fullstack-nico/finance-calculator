fc4e8baa79ae3b3e4397c966056c15897ba581c4
hafizfqb <63943967+fullstack-nico@users.noreply.github.com>
2022-08-30 13:35:21 +0800

    Finished dashboard page design

--- src/modules/main/screen/Dashboard.js
+++ src/modules/main/screen/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StyleSheet, useWindowDimensions } from 'react-native';
 import {Text} from '../../../component/typography';
 import {Container} from '../component';
 import {
@@ -6,9 +7,21 @@ import {
     ChevronDownIcon,
     Divider,
 } from 'native-base';
+import {theme} from '../../../_config/global';
+import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis, VictoryTooltip, VictoryLabel, VictoryVoronoiContainer} from "victory-native";
 
+const data = [
+    { datetime: 'mon', amount: 120.51, label: "120.51" },
+    { datetime: 'tue', amount: 98.33, label: "98.33" },
+    { datetime: 'wed', amount: 43.22 , label: "43.22" },
+    { datetime: 'thu', amount: 24.01 , label: "24.01" },
+    { datetime: 'fri', amount: 4.02 , label: "4.02" },
+    { datetime: 'sat', amount: 54.55 , label: "54.55" },
+    { datetime: 'sun', amount: 140.01 , label: "140.01" },
+];
 
 export default function Dashboard(){
+    const { height, width } = useWindowDimensions();
     return(
         <Container>
             <Box style={{ borderWidth: 0.5, padding: 10}}>
@@ -36,7 +49,7 @@ export default function Dashboard(){
 
             <Text style={{marginTop: 25, alignSelf: 'center', }}>Expenses Graph</Text>
 
-            <Box style={{ justifyContent: 'space-between', flexDirection: 'row', marginTop: 10}}>
+            <Box style={{ justifyContent: 'space-between', flexDirection: 'row', marginTop: 10, marginBottom: 20}}>
                 <Box style={{flex:1}}>
                     <Text style={{alignSelf: 'center'}}>11 Feb</Text>
                 </Box>
@@ -51,6 +64,56 @@ export default function Dashboard(){
                 </Box>
             </Box>
 
+            <Text style={{marginBottom: -30, marginLeft: 5, fontSize: theme.FONT_SIZE_SMALL}}>Expense (RM)</Text>
+            <VictoryChart
+                padding={{ top: 40, bottom: 80, left: 40, right: 20 }}
+                domainPadding={{x:32, y:30}}
+                containerComponent={
+                    <VictoryVoronoiContainer
+                        labels={({ datum }) => `RM: ${datum.amount}`}
+                    />
+                }
+                width={width}
+                height={300}
+            >
+                <VictoryLine
+
+                    style={{
+                        data: { stroke: "#87ceeb" },
+                    }}
+                    data={data}
+                    x={"datetime"}
+                    y={"amount"}
+
+
+                />
+
+                <VictoryAxis
+                    label="Days"
+                    style={{
+                        axisLabel: { padding: 35 }
+                    }}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    label="RM"
+                    style={{
+                    axisLabel: { padding: 50 },
+                    }}
+
+                />
+            </VictoryChart>
+
+
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5fcff"
+    }
+});
