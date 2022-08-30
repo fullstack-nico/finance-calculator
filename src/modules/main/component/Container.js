a60752827960812e0d7ad37d9c54c347844167ca
hafizfqb <63943967+fullstack-nico@users.noreply.github.com>
2022-08-30 10:59:53 +0800

    Before installing Victory chart

--- src/modules/main/component/Container.js
+++ src/modules/main/component/Container.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, ScrollView} from 'react-native';
+import {theme} from '../../../_config/global';
 
 export function Container({children}){
     return(
-        <View style={{margin: 5}}>
+        <View style={{margin: theme.MARGIN_MEDIUM, marginTop: theme.MARGIN_SMALL}}>
             <ScrollView>
                 {children}
             </ScrollView>
--- src/modules/main/screen/Dashboard.js
+++ src/modules/main/screen/Dashboard.js
@@ -4,6 +4,7 @@ import {Container} from '../component';
 import {
     Box,
     ChevronDownIcon,
+    Divider,
 } from 'native-base';
 
 
@@ -21,18 +22,35 @@ export default function Dashboard(){
                 <Text>Daily Spending</Text>
                 <Text>RM 10.00</Text>
             </Box>
-
-            <Box flexDirection='row' justifyContent='space-between' marginTop='5'>
+            <Divider marginTop={2}/>
+            <Box flexDirection='row' justifyContent='space-between' marginTop='2'>
                 <Text>Weekly Spending</Text>
                 <Text>RM 110.00</Text>
             </Box>
-
-            <Box flexDirection='row' justifyContent='space-between' marginTop='5'>
+            <Divider marginTop={2}/>
+            <Box flexDirection='row' justifyContent='space-between' marginTop='2'>
                 <Text>Monthly Budget</Text>
                 <Text>RM 110.00 / 500.00</Text>
             </Box>
+            <Divider marginTop={2}/>
 
-            <Text style={{marginTop: 20, alignSelf: 'center', }}>Expenses Graph</Text>
+            <Text style={{marginTop: 25, alignSelf: 'center', }}>Expenses Graph</Text>
+
+            <Box style={{ justifyContent: 'space-between', flexDirection: 'row', marginTop: 10}}>
+                <Box style={{flex:1}}>
+                    <Text style={{alignSelf: 'center'}}>11 Feb</Text>
+                </Box>
+
+                <Divider orientation="vertical"/>
+                <Box style={{flex:1}}>
+                    <Text style={{alignSelf: 'center'}}>Week 2</Text>
+                </Box>
+                <Divider orientation="vertical"/>
+                <Box style={{flex:1}}>
+                    <Text style={{alignSelf: 'center'}}>Day 7 - 13</Text>
+                </Box>
+            </Box>
+
         </Container>
     )
 }
